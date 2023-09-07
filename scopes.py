022a32b32b3906262bdb2d883a34d8990f500fc1
Gbenga Adeyi <adeyigbenga005@gmail.com>
2023-09-07 19:32:37 +0100

    Update scopes

--- scopes.py
+++ scopes.py
@@ -5,10 +5,13 @@ from models import UserType
 
 class APIScope(str, Enum):
     ALL = "all"  # access to all endpoints
+    CREATE_USER = "create_user"
+    ORGANIZATION_READ = "organization:read"
+    ORGANIZATION_WRITE = "organization:write"
 
 
-DEFAULT_USER_SCOPES: dict[UserType, set[set]] = {
+DEFAULT_USER_SCOPES: dict[UserType, set[APIScope]] = {
     UserType.PLIXA_SUPERUSER: {APIScope.ALL},
-    UserType.PLIXA_STAFF: set(),
-    UserType.PLATFORM_USER: set(),
+    UserType.PLIXA_STAFF: {APIScope.ORGANIZATION_READ, APIScope.ORGANIZATION_WRITE},
+    UserType.PLATFORM_USER: {APIScope.ORGANIZATION_READ, APIScope.ORGANIZATION_WRITE},
 }
