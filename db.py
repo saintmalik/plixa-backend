da365a3393168c35359d77cf4b4286b451061858
Gbenga Adeyi <adeyigbenga005@gmail.com>
2023-09-07 19:33:15 +0100

    Implement get_collection

--- db.py
+++ db.py
@@ -1,12 +1,9 @@
-import logging
 from enum import Enum
 
 import motor.motor_asyncio
 
 from settings import default_settings
 
-logger = logging.getLogger(__name__)
-logger.error(default_settings.MONGODB_URL)
 client = motor.motor_asyncio.AsyncIOMotorClient(default_settings.MONGODB_URL)
 
 default_db = client.plixa_db
@@ -14,6 +11,9 @@ default_db = client.plixa_db
 
 class DBCollection(str, Enum):
     USER = "users"
+    ORGANIZATION = "organizations"
+    CLUSTER = "clusters"
 
 
-user_collection = default_db.get_collection(DBCollection.USER)
+def get_collection(collection_name: DBCollection):
+    return default_db.get_collection(collection_name)
